a2be541a32a974fcc70fc07fed0c67f7c948d53e
BugHunter-X <askzenai@gmail.com>
2024-12-16 10:12:10 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,34 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* ...more routes */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/about">Go to About</Link>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      {/* This link is problematic */}
+      <Link to="/">Go back to Home</Link>
+    </div>
+  );
+}
+```
